84ccf9b99e252b9ca8ff07d40c1ccefa2eb6af4c
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2023-07-23 20:53:57 -0300

    [add] main search dropdown list

--- app/components/mainSearch/index.tsx
+++ app/components/mainSearch/index.tsx
@@ -1,23 +1,72 @@
 "use client";
 
-import { Box, IconButton, Input, InputAdornment } from "@mui/material";
+import {
+  Box,
+  ClickAwayListener,
+  IconButton,
+  Input,
+  InputAdornment,
+  MenuItem,
+  MenuList,
+  Paper,
+} from "@mui/material";
 import { alpha } from "@mui/material/styles";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+import debounce from "lodash.debounce";
 
-import React from "react";
+import React, { useMemo, useRef, useState } from "react";
+import Link from "next/link";
 
 export default function MainSearch() {
+  const inputRef = useRef<string>("");
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [value, setValue] = useState<string>("");
+  const searchHandler = () => {
+    if (inputRef.current.length >= 3) {
+      setIsOpen(true);
+      console.log("ref value", inputRef.current);
+    } else {
+      setIsOpen(false);
+
+    }
+  };
+  const debouncedChangeHandler = useMemo(
+    () => debounce(searchHandler, 1000),
+    []
+  );
+  const getHighlightedText = (text: string, highlight: string) => {
+    // Split on highlight term and include term into parts, ignore case
+    const parts = text.split(new RegExp(`(${highlight})`, "gi"));
+    return (
+      <span>
+        {parts.map((part, i) => (
+          <span
+            key={i}
+            style={
+              part.toLowerCase() === highlight.toLowerCase()
+                ? { fontWeight: "bold" }
+                : {}
+            }
+          >
+            {part}
+          </span>
+        ))}{" "}
+      </span>
+    );
+  };
   return (
     <Box
       sx={{
         width: "100%",
         maxWidth: "400px",
+        position: "relative",
         "& > .MuiInput-root": {
           width: "100%",
         },
       }}
     >
       <Input
+        ref={inputRef}
         sx={(theme) => ({
           "& .MuiInput-root": {
             width: "100%",
@@ -54,17 +103,49 @@ export default function MainSearch() {
           },
         })}
         placeholder="Buscar"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          debouncedChangeHandler();
+          setValue(e.target.value);
+          inputRef.current = e.target.value;
+        }}
         endAdornment={
-          <InputAdornment position="end" sx={{position:"absolute", right:'1rem', top:'1.4rem'}}>
-            <IconButton
-              aria-label="toggle password visibility"
-              edge="end"
-            >
-              <SearchIcon/>
+          <InputAdornment
+            position="end"
+            sx={{ position: "absolute", right: "1rem", top: "1.4rem" }}
+          >
+            <IconButton aria-label="toggle password visibility" edge="end">
+              <SearchIcon />
             </IconButton>
           </InputAdornment>
         }
       />
+      {isOpen && (
+        <ClickAwayListener onClickAway={() => setIsOpen(false)}>
+          <Paper
+            sx={{
+              borderRadius: 4,
+              padding: 2,
+              position: "absolute",
+              bottom: 0,
+              left: 0,
+              width: "100%",
+              transform: "translateY(100%) translateY(5px)",
+            }}
+          >
+            <MenuList>
+              <MenuItem href={"/detalle-del-blog/1"} component={Link}>
+                {getHighlightedText("Mis primeras practicas", value)}
+              </MenuItem>
+              <MenuItem href={"/detalle-del-blog/1"} component={Link}>
+              {getHighlightedText("Practicas en el hospital", value)}
+              </MenuItem>
+              <MenuItem href={"/detalle-del-blog/1"} component={Link}>
+                Post con un titulo similar a la busqueda
+              </MenuItem>
+            </MenuList>
+          </Paper>
+        </ClickAwayListener>
+      )}
     </Box>
   );
 }
