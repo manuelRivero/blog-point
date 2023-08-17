b4748605c091b43cde2e8ac3a2ec42d5b2e5029e
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2023-08-17 17:32:03 -0300

    [add] .env api  variable

--- app/client/index.tsx
+++ app/client/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosIntance = axios.create({
-    baseURL:"http://localhost:4000/api",
+    baseURL:process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true
 })
