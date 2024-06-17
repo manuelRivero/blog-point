0e104468874f86d3f30af9c4375ad3046a842a66
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2024-06-17 02:26:12 -0300

    [wip] pwa features

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
-
+// Configuration object tells the next-pwa plugin 
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
 const nextConfig = {
   reactStrictMode: false,
     sassOptions: {
@@ -11,4 +16,4 @@ const nextConfig = {
      },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
